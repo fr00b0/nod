666e7b34d011de51807faf764d1a524a1e7d0154
Fredrik Berggren <0x1@bitflags.com>
2016-12-06 21:18:18 +0100

    Add tests for move assignment

--- tests/tests/connection_tests.cpp
+++ tests/tests/connection_tests.cpp
@@ -121,6 +121,43 @@ SCENARIO( "Scoped connection objects are default constructible" ) {
 	}
 }
 
+SCENARIO( "Scoped connection objects are move constructible" ) {
+	GIVEN( "a signal with a connected slot, managed by a scoped connection" ) {
+		nod::signal<void()> signal;
+		std::ostringstream ss;
+		nod::scoped_connection scoped1 = signal.connect([&](){ ss << "signaled!"; });
+		WHEN( "a new scoped connection can be move assigned from the original scoped connection" ) {
+			nod::scoped_connection scoped2 = std::move(scoped1);
+			THEN( "the new connection is connected" ) {
+				REQUIRE( scoped2.connected() == true );
+			}
+			AND_WHEN( "firing the signal triggers the slot only once" ) {
+				signal();
+				REQUIRE( ss.str() == "signaled!" );
+			}
+		}
+	}
+	GIVEN( "two signals connected to a slot, managed by two scoped connections" ) {
+		nod::signal<void()> signal;
+		std::ostringstream ss;
+		nod::scoped_connection connection1 = signal.connect([&](){ ss << "1"; });
+		nod::scoped_connection connection2 = signal.connect([&](){ ss << "2"; });
+		REQUIRE( signal.slot_count() == 2 );
+		WHEN( "we move assign the first of the connections with the second one" ) {
+			connection1 = std::move(connection2);
+			THEN( "the signal only has one connected slot") {
+				REQUIRE( signal.slot_count() == 1 );
+			}
+			AND_WHEN( "we trigger the signal" ) {
+				signal();
+				THEN( "only the second slot is executed" ) {
+					REQUIRE( ss.str() == "2" );
+				}
+			}
+		}
+	}
+}
+
 SCENARIO( "Scoped connection will disconnect when destroyed" ) {
 	GIVEN( "A signal" ) {
 		std::ostringstream ss;
