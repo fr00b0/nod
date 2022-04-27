2ea9e94a882cd55c2f3ed303ffbc404bb8f5b078
darife <darife@jlg.com>
2022-04-27 07:34:25 -0400

    feat: add definitions header

---
+++ include/nod/noddefs.hpp
@@ -0,0 +1,12 @@
+#ifndef IG_NOD_INCLUDE_NODDEFS_HPP
+#define IG_NOD_INCLUDE_NODDEFS_HPP
+
+#ifndef signals 
+    #define signals public
+#endif // signals 
+
+#ifndef slots
+    #define slots
+#endif // slots
+
+#endif // IG_NOD_INCLUDE_NODDEFS_HPP
